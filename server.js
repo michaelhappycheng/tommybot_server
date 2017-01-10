0c5b84a7facdb9524785b4aec0bf4b0c948cd8da
justinhe16 <hejustin@usc.edu>
2017-01-10 04:43:13 -0800

    fixed date ranges

--- server.js
+++ server.js
@@ -294,7 +294,7 @@ function apiaiCall(text, sender) {
             }
             var dates = [];
             dates.push(date);
-            if (response.result.parameters['date-period'] != 'today') {
+            else if (response.result.parameters['date-period'] != 'today') {
               for (var i = 0; i < 6; i++) {
                 dates.push(formatDateYY(d.setDate(d.getDate() + 1)));
               }
