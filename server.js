4a17b339e3786f2ded8957b75d574c992e049ec7
justinhe16 <hejustin@usc.edu>
2017-01-10 04:11:48 -0800

    fixed small error

--- server.js
+++ server.js
@@ -891,7 +891,7 @@ function sendEventsChoiceCard(senderID, calendarName) {
 function sendEventsCard(sender, eventStats) {
 
     if (eventStats.length == 0) {
-      SendTextmessage(sender, "Sorry, there no events for that calandar for that specified section of time.");
+      SendTextMessage(sender, "Sorry, there no events for that calandar for that specified section of time.");
     }
 
     else {
