fd97c85773dab9d8da5e0d33bd0a2d98efea682b
Samuel Breck <breck@usc.edu>
2017-02-10 17:11:03 -0800

    cleanup splitting files, added sendText Message

--- js/cards.js
+++ js/cards.js
@@ -387,4 +387,29 @@ sendHeadlinesCard: function sendHeadlinesCard(sender, eventStats) {
     }
 }
 
+function sendTextMessage(sender, text) {
+    messageData = {
+        text: text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: token
+        },
+        method: 'POST',
+        json: {
+            recipient: {
+                id: sender
+            },
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    })
+}
+
 }
